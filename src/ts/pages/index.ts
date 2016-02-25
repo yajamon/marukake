2f52a6152ddb16c30434a4ea956a4e878c9c1afc
yajamon <yajamon.tatsuki@gmail.com>
2016-02-25 11:55:52 +0900

    Add event listener to main table

--- src/ts/pages/index.ts
+++ src/ts/pages/index.ts
@@ -1,3 +1,12 @@
 window.addEventListener("load", function(){
     document.body.appendChild(document.createTextNode("Hello typescript world!"));
+    
+    let gamePanel = document.querySelector('.gamePanel');
+    gamePanel.addEventListener("click", (event)=>{
+        let target = event.target as HTMLElement;
+        if (target.nodeName != "TD") {
+            return ;
+        }
+        event.preventDefault();
+    });
 });
