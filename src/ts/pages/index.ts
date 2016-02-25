c76698d6b99d6d9c5565cd384fd37a7629b8575e
yajamon <yajamon.tatsuki@gmail.com>
2016-02-25 12:30:52 +0900

    Remove hello world

--- src/ts/pages/index.ts
+++ src/ts/pages/index.ts
@@ -1,6 +1,4 @@
 window.addEventListener("load", function(){
-    document.body.appendChild(document.createTextNode("Hello typescript world!"));
-    
     let gamePanel = document.querySelector('.gamePanel');
     gamePanel.addEventListener("click", (event)=>{
         let target = event.target as HTMLElement;
