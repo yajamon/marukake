1089c2583ebee8145a69e2f97d25c743850e5301
yajamon <yajamon.tatsuki@gmail.com>
2016-02-25 12:32:41 +0900

    Update main script

--- src/ts/pages/index.ts
+++ src/ts/pages/index.ts
@@ -1,10 +1,12 @@
 window.addEventListener("load", function(){
     let gamePanel = document.querySelector('.gamePanel');
-    gamePanel.addEventListener("click", (event)=>{
-        let target = event.target as HTMLElement;
-        if (target.nodeName != "TD") {
-            return ;
-        }
-        event.preventDefault();
-    });
+    if (gamePanel) {
+        gamePanel.addEventListener("click", (event)=>{
+            let target = event.target as HTMLElement;
+            if (target.nodeName != "TD") {
+                return ;
+            }
+            event.preventDefault();
+        });
+    }
 });
