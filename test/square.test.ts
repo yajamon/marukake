3b552ac06eb1be64a52455bcb2ce867f1eccbfdf
yajamon <yajamon.tatsuki@gmail.com>
2016-03-04 22:23:28 +0900

    Update square test code

--- test/square.test.ts
+++ test/square.test.ts
@@ -8,8 +8,8 @@ describe("Square", ()=>{
     describe(".state", ()=>{
         it("is EMPTY when create", ()=>{
             let square = new MainApp.Square();
-            expect(square.state).to.not.be.undefined
-                .and.equal(MainApp.SQUARE_STATE.EMPTY);
+            expect(square.state).to.not.be.undefined;
+            expect(square.state).to.equal(MainApp.SQUARE_STATE.EMPTY);
         });
     });
 });
