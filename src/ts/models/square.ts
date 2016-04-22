736eb2b29a82649a865c197bf1d9f09d34c3d672
yajamon <yajamon.tatsuki@gmail.com>
2016-04-22 13:35:03 +0900

    Remove SQUARE_STATE

--- src/ts/models/square.ts
+++ src/ts/models/square.ts
@@ -1,14 +1,9 @@
 namespace MainApp {
-    export enum SQUARE_STATE {
-        EMPTY,
-    }
     /**
      * Square
      */
     export class Square {
-        state: SQUARE_STATE;
         constructor() {
-            this.state = SQUARE_STATE.EMPTY;
         }
     }
 }
