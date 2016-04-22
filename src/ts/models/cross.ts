0a842bd12ede540bb39baacb011dfb385775ffd3
yajamon <yajamon.tatsuki@gmail.com>
2016-04-22 13:49:02 +0900

    Add Cross class

---
+++ src/ts/models/cross.ts
@@ -0,0 +1,11 @@
+/// <reference path="./interfaces/puttable.ts" />
+
+namespace MainApp {
+    /**
+     * Cross
+     */
+    export class Cross implements Puttable {
+        constructor() {
+        }
+    }
+}
