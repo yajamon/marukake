ce978d65a0a64686aaf571b18a68b5be0ba783d4
yajamon <yajamon.tatsuki@gmail.com>
2016-04-22 13:47:10 +0900

    Add Circle class

---
+++ src/ts/models/circle.ts
@@ -0,0 +1,11 @@
+/// <reference path="./interfaces/puttable.ts" />
+
+namespace MainApp {
+    /**
+     * Circle
+     */
+    export class Circle implements Puttable {
+        constructor() {
+        }
+    }
+}
