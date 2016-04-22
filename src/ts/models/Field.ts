a2038e86ec66e79b39aca1e60fca9c1f4659ad34
yajamon <yajamon.tatsuki@gmail.com>
2016-04-22 20:53:55 +0900

    Add size property to Field

--- src/ts/models/Field.ts
+++ src/ts/models/Field.ts
@@ -1,17 +1,30 @@
 /// <reference path="./Square.ts" />
+/// <reference path="./interfaces/Size.ts" />
+
 
 namespace MainApp {
     /**
      * Field
      */
     export class Field {
-        data:Square[][];
-        constructor() {
+        private _size: Size;
+        data: Square[];
+
+        constructor(size: Size) {
+            this._size = size;
             this.data = [
                 [new Square(), new Square(), new Square()],
                 [new Square(), new Square(), new Square()],
                 [new Square(), new Square(), new Square()],
             ];
         }
+
+        public get size(): Size {
+            return this._size;
+        }
+        public set size(v: Size) {
+            throw "Can't write property";
+        }
+
     }
 }
