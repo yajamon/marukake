bd50bbb1584bab84c74f997532ac49ce261ccae1
yajamon <yajamon.tatsuki@gmail.com>
2016-04-22 13:22:56 +0900

    Remove test of square.state

--- test/square.test.ts
+++ test/square.test.ts
@@ -4,12 +4,6 @@
 
 var expect = chai.expect;
 
-describe("Square", ()=>{
-    describe(".state", ()=>{
-        it("is EMPTY when create", ()=>{
-            let square = new MainApp.Square();
-            expect(square.state).to.not.be.undefined;
-            expect(square.state).to.equal(MainApp.SQUARE_STATE.EMPTY);
-        });
-    });
+describe("Square", () => {
+
 });
