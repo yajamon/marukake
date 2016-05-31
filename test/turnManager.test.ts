1da21dca6ae8baff24ca95c45c096139a7c89654
yajamon <yajamon.tatsuki@gmail.com>
2016-05-31 18:50:15 +0900

    Add TurnManager test

---
+++ test/turnManager.test.ts
@@ -0,0 +1,20 @@
+/// <reference path="../typings/mocha/mocha.d.ts" />
+/// <reference path="../typings/chai/chai.d.ts" />
+
+var expect = chai.expect;
+
+describe("TurnManager", () => {
+    var manager: MainApp.TurnManager;
+
+    beforeEach(() => {
+        manager = new MainApp.TurnManager();
+    });
+
+    describe("#currentTurn", () => {
+
+    });
+
+    describe("#turnEnd", () => {
+
+    });
+});
