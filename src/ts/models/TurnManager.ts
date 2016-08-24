454623cbb8d49c1eaf2148c51f7f826ac73a573d
yajamon <yajamon.tatsuki@gmail.com>
2016-08-24 17:39:11 +0900

    Edit TurnManager model
    implement #turnEnd

--- src/ts/models/TurnManager.ts
+++ src/ts/models/TurnManager.ts
@@ -3,14 +3,23 @@ namespace MainApp {
      * TurnManager
      */
     export class TurnManager {
+        private turn: number;
         constructor() {
+            this.turn = 1;
         }
 
         /**
          * currentTurn
          */
         public currentTurn() {
-            return 1;
+            return this.turn;
+        }
+
+        /**
+         * turnEnd
+         */
+        public turnEnd() {
+            this.turn += 1;
         }
     }
 }
